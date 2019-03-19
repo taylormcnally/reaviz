94a84ebfd4c84b1dcec6ead1cf07bcd9c5d48ec8
Austin McDaniel <amcdaniel2@gmail.com>
2019-03-19 11:28:22 -0500

    add more demos

--- src/RadialBarChart/RadialBarChart.story.tsx
+++ src/RadialBarChart/RadialBarChart.story.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { RadialBarChart } from './RadialBarChart';
 import { largeCategoryData } from '../common/demo';
+import { number, boolean, withKnobs, object, array } from '@storybook/addon-knobs';
+import { sequentialScheme } from '../common/utils/color';
+import { RadialBarSeries } from './RadialBarSeries';
 
 storiesOf('Charts/Bar/Radial', module)
- .add('Simple', () => (
-    <RadialBarChart
-      height={300}
-      width={300}
-      data={largeCategoryData}
-    />
+  .addDecorator(withKnobs)
+  .add('Simple', () => {
+    const innerRadius = number('Inner Radius', 80);
+    const animated = boolean('Animated', true);
+    const colorScheme = array('Color Scheme', sequentialScheme);
+    const data = object('Data', largeCategoryData);
+
+    return (
+      <RadialBarChart
+        height={300}
+        width={300}
+        innerRadius={innerRadius}
+        data={data}
+        series={
+          <RadialBarSeries
+            animated={animated}
+            colorScheme={colorScheme}
+          />
+        }
+      />
+    );
+  }, { options: { showAddonPanel: true } })
+  .add('Resizable', () => (
+    <div style={{ width: '50vw', height: '75vh', border: 'solid 1px red' }}>
+      <RadialBarChart
+        data={largeCategoryData}
+      />
+    </div>
   ));
