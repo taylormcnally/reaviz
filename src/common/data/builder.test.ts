0014b91b7738c4b6788e3d693fcc585832b8a243
Austin McDaniel <amcdaniel2@gmail.com>
2019-04-30 16:59:57 -0400

    add builder test

---
+++ src/common/data/builder.test.ts
@@ -0,0 +1,9 @@
+import { buildShallowChartData } from './builder';
+import { categoryData } from '../demo/category';
+
+ describe('Builder', () => {
+  it('it should build zero based chart', () => {
+    const result = buildShallowChartData(categoryData);
+    expect(result[0].x0).toBe(0);
+  });
+});
