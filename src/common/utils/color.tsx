0caf00050098b7cc0af0c117b4ac94b5adab962f
Stephanie Yang <steppy452@gmail.com>
2019-06-29 18:16:21 -0500

    multiseries bar chart colors are based on num of series instead of data points

--- src/common/utils/color.tsx
+++ src/common/utils/color.tsx
@@ -7,6 +7,12 @@ export const sequentialScheme = ['#3ec4e8'];
  * Get a color given a range.
  */
 export function getColor(colorScheme: string[], data: any[]) {
-  const dataRange = range(data.length).map(r => r.toString());
+  const isMultiSeries = data[0].hasOwnProperty('data');
+  let dataRange;
+  if(isMultiSeries) {
+    dataRange = range(data[0]['data'].length).map(r => r.toString());
+  } else {
+    dataRange = range(data.length).map(r => r.toString());
+  }
   return scaleOrdinal(colorScheme).domain(dataRange);
 }
