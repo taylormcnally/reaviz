dc5d385ebab17237cd5da946ddebbecc1a77dc18
amcdnl <amcdaniel2@gmail.com>
2019-10-08 16:26:22 -0400

    finally…

--- .circleci/publish-dev-builds.js
+++ .circleci/publish-dev-builds.js
@@ -18,8 +18,8 @@ const { execSync } = require('child_process');
   // construct new version from base version 2.0.0 to become 2.0.0+dev.shortsha
   const version = parse(json.version);
   const newVersion = `${version.major}.${version.minor}.${version.patch}-dev.master-${commit}`;
-  console.info('publishing new version', newVersion);
+  console.info('Publishing new version', newVersion);
 
-  const output = execSync(`yarn publish --access public --non-interactive --no-git-tag-version --new-version ${version} --tag dev`);
+  const output = execSync(`yarn publish --access public --non-interactive --no-git-tag-version --new-version ${newVersion} --tag dev`);
   console.info(`Published Dev /r/n -> ${output}`);
 })();
