9561ae317a68bf2e16294bf75a7d3ae3324fa4e7
amcdnl <amcdaniel2@gmail.com>
2019-10-08 11:42:55 -0400

    so beautiful!

---
+++ docs/tools/Sandbox.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import CodeSandboxer from 'react-codesandboxer';
+
+export const Sandbox = ({ name, path, description }) => (
+  <CodeSandboxer
+    name={name}
+    pkgJSON={{
+      name: name.split(' ').join('-'),
+      version: '0.0.0',
+      description,
+      main: 'index.js',
+      dependencies: {
+        'react': 'latest',
+        'reaviz': 'latest',
+        'react-dom': 'latest'
+      }
+    }}
+    examplePath={path}
+    gitInfo={{
+      account: 'jask-oss',
+      repository: 'reaviz',
+      host: 'github'
+    }}
+    autoDeploy={true}
+    preload={true}
+    skipRedirect={true}
+  >
+    {({ sandboxId, loading }) => (
+      <div>
+        {loading && !sandboxId ? null : (
+          <iframe
+            src={`https://codesandbox.io/embed/${sandboxId}?fontsize=14`}
+            title="codesandboxer-example"
+            allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+            style={{ width: '100%', height: '500px', border: 0, overflow: 'hidden' }}
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin">
+          </iframe>
+        )}
+      </div>
+    )}
+  </CodeSandboxer>
+)
