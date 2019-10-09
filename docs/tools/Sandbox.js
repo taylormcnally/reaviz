43c20fbf0d3c245869000fc7b1c06c955bafbd72
amcdnl <amcdaniel2@gmail.com>
2019-10-09 14:57:50 -0400

    fix. titles [skip ci]

--- docs/tools/Sandbox.js
+++ docs/tools/Sandbox.js
@@ -23,7 +23,6 @@ export const Sandbox = ({ name, path, description, dependencies }) => (
       host: 'github'
     }}
     autoDeploy={true}
-    preload={true}
     skipRedirect={true}
   >
     {({ sandboxId, loading }) => (
@@ -31,7 +30,7 @@ export const Sandbox = ({ name, path, description, dependencies }) => (
         {loading && !sandboxId ? null : (
           <iframe
             src={`https://codesandbox.io/embed/${sandboxId}?fontsize=14`}
-            title="codesandboxer-example"
+            title={name}
             allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
             style={{ width: '100%', height: '500px', border: 0, overflow: 'hidden' }}
             sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin">
