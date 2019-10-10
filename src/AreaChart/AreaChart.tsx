c3f5d64afa26bd16805813225b1b0ae96f2fd6b7
amcdnl <amcdaniel2@gmail.com>
2019-10-10 15:13:21 -0400

    testing out some docs

--- src/AreaChart/AreaChart.tsx
+++ src/AreaChart/AreaChart.tsx
@@ -37,14 +37,45 @@ import { CloneElement } from '../common/utils/children';
 import memoize from 'memoize-one';
 
 export interface AreaChartProps extends ChartProps {
+  /**
+   * Data the chart will recieve to render.
+   */
   data: ChartDataShape[];
+
+  /**
+   * The series component that renders the area/line/circles components.
+   */
   series: ReactElement<AreaSeriesProps, typeof AreaSeries>;
+
+  /**
+   * The linear axis component for the Y Axis of the chart.
+   */
   yAxis: ReactElement<LinearAxisProps, typeof LinearAxis>;
+
+  /**
+   * The linear axis component for the X Axis of the chart.
+   */
   xAxis: ReactElement<LinearAxisProps, typeof LinearAxis>;
+
+  /**
+   * The chart's background gridlines component.
+   */
   gridlines: ReactElement<GridlineSeriesProps, typeof GridlineSeries> | null;
+
+  /**
+   * The chart's brush component.
+   */
   brush: ReactElement<ChartBrushProps, typeof ChartBrush> | null;
+
+  /**
+   * The chart's zoom pan component.
+   */
   zoomPan: ReactElement<ChartZoomPanProps, typeof ChartZoomPan> | null;
-  secondaryAxis?: JSX.Element[];
+
+  /**
+   * Any secondary axis components. Useful for multi-axis charts.
+   */
+  secondaryAxis?: ReactElement<LinearAxisProps, typeof LinearAxis>[];
 }
 
 interface AreaChartState {
