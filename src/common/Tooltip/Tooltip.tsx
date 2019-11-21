6ed01e825a3e25b55faf047ee1a0dfbc1d6d0316
amcdnl <amcdaniel2@gmail.com>
2019-11-21 09:11:15 -0500

    moar fcs

--- src/common/Tooltip/Tooltip.tsx
+++ src/common/Tooltip/Tooltip.tsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { FC, useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
-import bind from 'memoize-bind';
 import {
   Placement,
   ReferenceObject,
@@ -10,7 +9,7 @@ import {
 import css from './Tooltip.module.scss';
 import { motion } from 'framer-motion';
 
-const tooltips: Tooltip[] = [];
+const tooltips: ((setter: boolean) => void)[] = [];
 
 export interface TooltipProps {
   /**
@@ -69,109 +68,96 @@ export interface TooltipProps {
   followCursor?: boolean;
 }
 
-interface TooltipState {
-  visible: boolean;
-}
+export const Tooltip: FC<Partial<TooltipProps>> = ({
+  className,
+  children,
+  content,
+  disabled = false,
+  enterDelay = 0,
+  leaveDelay = 200,
+  placement = 'top',
+  trigger = 'hover',
+  visible = false,
+  followCursor = false,
+  ...rest
+}) => {
+  const [internalVisible, setInternalVisible] = useState<boolean>(visible);
+  const timeout = useRef<any>();
+  const mounted = useRef<boolean>();
+  const ref = useRef<(setter: boolean) => void>(setInternalVisible);
 
-export class Tooltip extends Component<TooltipProps, TooltipState> {
-  static defaultProps: Partial<TooltipProps> = {
-    enterDelay: 0,
-    leaveDelay: 200,
-    placement: 'top',
-    trigger: 'hover',
-    visible: false,
-    followCursor: false
-  };
-
-  timeout: any;
-
-  state: TooltipState = {
-    visible: this.props.visible
-  };
-
-  componentDidUpdate(prevProps: TooltipProps) {
-    const { visible } = this.props;
-
-    if (visible !== prevProps.visible && visible !== this.state.visible) {
-      if (visible) {
-        this.activate();
-      } else {
-        this.deactivate();
-      }
+  useEffect(() => {
+    // componentDidUpdateLogic style logic
+    if (!mounted.current) {
+      mounted.current = true;
+    } else {
+      setInternalVisible(visible);
     }
-  }
 
-  componentWillUnmount() {
-    this.deactivate();
-  }
+    return () => {
+      clearTimeout(timeout.current);
+      deactivate();
+    };
+  });
 
-  onActivate() {
-    if (!this.state.visible) {
-      this.deactivateAll();
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(() => this.activate(), this.props.enterDelay);
-    }
-  }
+  const deactivateAll = () =>
+    tooltips.forEach((r, i) => {
+      r(false);
+      tooltips.splice(i, 1);
+    });
 
-  onDeactivate() {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => this.deactivate(), this.props.leaveDelay);
-  }
-
-  activate() {
-    if (!this.props.disabled) {
-      this.setState({ visible: true });
-      tooltips.push(this);
-    }
-  }
-
-  deactivate() {
-    const idx = tooltips.indexOf(this);
+  const deactivate = () => {
+    const idx = tooltips.indexOf(ref.current);
     if (idx > -1) {
-      this.setState({ visible: false });
+      setInternalVisible(false);
       tooltips.splice(idx, 1);
     }
-  }
-
-  deactivateAll() {
-    tooltips.forEach(t => t.deactivate());
-  }
-
-  renderContent = () => {
-    const { content, className } = this.props;
-    const children = typeof content === 'function' ? content() : content;
-
-    if (!children) {
-      return null;
-    }
-
-    return (
-      <motion.div
-        className={classNames(css.tooltip, className)}
-        initial={{ opacity: 0, scale: 0.3 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.3 }}
-      >
-        {children}
-      </motion.div>
-    );
   };
 
-  render() {
-    const { children, content, className, visible, ...rest } = this.props;
+  return (
+    <ConnectedOverlay
+      {...rest}
+      placement={placement}
+      trigger={trigger}
+      followCursor={followCursor}
+      visible={internalVisible}
+      content={() => {
+        const contentChildren =
+          typeof content === 'function' ? content() : content;
 
-    return (
-      <ConnectedOverlay
-        {...rest}
-        visible={this.state.visible}
-        content={this.renderContent}
-        closeOnBodyClick={false}
-        closeOnEscape={false}
-        onActivate={bind(this.onActivate, this)}
-        onDeactivate={bind(this.onDeactivate, this)}
-      >
-        {children}
-      </ConnectedOverlay>
-    );
-  }
-}
+        if (!contentChildren) {
+          return null;
+        }
+
+        return (
+          <motion.div
+            className={classNames(css.tooltip, className)}
+            initial={{ opacity: 0, scale: 0.3 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.3 }}
+          >
+            {contentChildren}
+          </motion.div>
+        );
+      }}
+      onActivate={() => {
+        if (!internalVisible) {
+          deactivateAll();
+          clearTimeout(timeout.current);
+          timeout.current = setTimeout(() => {
+            if (!disabled) {
+              setInternalVisible(true);
+              tooltips.push(ref.current);
+            }
+          }, enterDelay);
+        }
+      }}
+      onDeactivate={() => {
+        clearTimeout(timeout.current);
+        timeout.current = setTimeout(() => deactivate(), leaveDelay);
+      }}
+    >
+      {children}
+    </ConnectedOverlay>
+  );
+};
--- src/common/Tooltip/TooltipTemplate.tsx
+++ src/common/Tooltip/TooltipTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { Fragment, FC } from 'react';
 import { formatValue } from '../utils/formatting';
 import { ChartInternalDataTypes } from '../data';
 import css from './TooltipTemplate.module.scss';
@@ -32,9 +32,16 @@ interface TooltipTemplateProps {
   className?: any;
 }
 
-export class TooltipTemplate extends PureComponent<TooltipTemplateProps> {
-  renderValues(data: SingleTooltipValue, index: number) {
-    const { color } = this.props;
+export const TooltipTemplate: FC<TooltipTemplateProps> = ({
+  value,
+  color,
+  className
+}) => {
+  if (!value) {
+    return null;
+  }
+
+  const renderValues = (data: SingleTooltipValue, index: number) => {
     const fill = color(data, index);
 
     return (
@@ -46,46 +53,38 @@ export class TooltipTemplate extends PureComponent<TooltipTemplateProps> {
         <span>{formatValue(data.value || data.y)}</span>
       </span>
     );
-  }
+  };
 
-  renderMultiple(value: MultipleTooltipValues) {
+  const renderMultiple = (value: MultipleTooltipValues) => {
     const excessCount = value.data.length - 15;
     const pagedValues = value.data.slice(0, 15);
 
     return (
       <Fragment>
         {pagedValues.map((point, i) => (
-          <Fragment key={i}>{this.renderValues(point, i)}</Fragment>
+          <Fragment key={i}>{renderValues(point, i)}</Fragment>
         ))}
         {excessCount > 0 && <div>...{excessCount} more...</div>}
       </Fragment>
     );
-  }
+  };
 
-  render() {
-    const { value, className } = this.props;
+  const isMultiple = Array.isArray((value as any).data);
 
-    if (!value) {
-      return null;
-    }
-
-    const isMultiple = Array.isArray((value as any).data);
-
-    return (
-      <div className={className}>
-        <div className={css.label}>{formatValue(value!.x)}</div>
-        <div className={css.value}>
-          {isMultiple && this.renderMultiple(value as MultipleTooltipValues)}
-          {!isMultiple && (
-            <Fragment>
-              {formatValue(
-                (value as SingleTooltipValue).value ||
-                  (value as SingleTooltipValue).y
-              )}
-            </Fragment>
-          )}
-        </div>
+  return (
+    <div className={className}>
+      <div className={css.label}>{formatValue(value!.x)}</div>
+      <div className={css.value}>
+        {isMultiple && renderMultiple(value as MultipleTooltipValues)}
+        {!isMultiple && (
+          <Fragment>
+            {formatValue(
+              (value as SingleTooltipValue).value ||
+                (value as SingleTooltipValue).y
+            )}
+          </Fragment>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
